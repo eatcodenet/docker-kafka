0701870a3201837013da73107427f48058bad31e
Ayub Malik <ayub.malik@gmail.com>
2016-03-29 23:06:31 +0100

    wip

--- start-kafka.sh
+++ start-kafka.sh
@@ -9,6 +9,7 @@ if [ ! -z "$ADVERTISED_HOST" ]; then
     echo "advertised host: $ADVERTISED_HOST"
     sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
 fi
+
 if [ ! -z "$ADVERTISED_PORT" ]; then
     echo "advertised port: $ADVERTISED_PORT"
     sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
